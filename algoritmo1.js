4405f43841ce8eff5e609ac48050e7c2b98838ad
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-04-30 20:27:57 -0500

    Upload

---
+++ algoritmo1.js
@@ -0,0 +1,50 @@
+for(var i=0; i<5;i++){   
+    console.log(i);
+ } //console outupt: 0,1,2,3,4
+
+ for(var i=0; i<5; i++)
+{
+   i = i + 1;  
+   console.log(i);
+}//console outupt: 1,2,3,4,5
+
+for(var i=0; i<5; i++)
+{
+   i = i + 3; 
+   console.log(i);
+}//console outupt: 3,6
+
+function y(num1, num2){   
+    return num1+num2;
+ }
+ console.log(y(2,3))
+ console.log(y(3,5))
+ //console outupt: 5,8
+
+ function y(num1, num2){
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(y(2,3))
+ console.log(y(3,5))
+ //console output: 2,5,3,8
+
+a = 15
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a;
+}
+b = y(10);
+console.log(b);
+ //console output: 15,10,10
+
+ a = 15;
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a*2;
+}
+b = y(10);
+console.log(b);
+ //console output: 15,10,,20
